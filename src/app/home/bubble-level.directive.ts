1ae36c90f321e8a7deafd582fd7c872106e2cc9e
puraxa <edinpurivatra@gmail.com>
2019-12-12 15:18:14 +0100

    added angle

--- src/app/home/bubble-level.directive.ts
+++ src/app/home/bubble-level.directive.ts
@@ -1,38 +1,41 @@
 import { Directive, ElementRef, Renderer2 } from '@angular/core';
 import { DeviceMotion } from '@ionic-native/device-motion/ngx';
 import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
+import { Platform } from '@ionic/angular';
 
 @Directive({
   selector: '[appBubbleLevel]'
 })
 export class BubbleLevelDirective {
 
-  constructor(private el: ElementRef, private renderer: Renderer2, deviceMotion: DeviceMotion, private screenOrientation: ScreenOrientation) {  
-    deviceMotion.watchAcceleration({frequency: 150}).subscribe(response => {
-      if(response.z > 6){
-        renderer.setStyle(el.nativeElement, 'transform', `translate(${response.x * 40}px,${response.y*-40}px)`);
-        if(Math.abs(response.x * 40) + Math.abs(response.y * 40) > 190){
-          renderer.setStyle(el.nativeElement,'display','none');
-        }else {
-          renderer.setStyle(el.nativeElement,'display','initial');
+  constructor(private el: ElementRef, private renderer: Renderer2, deviceMotion: DeviceMotion, private screenOrientation: ScreenOrientation, private platform: Platform) {  
+    platform.ready().then(() => {
+      deviceMotion.watchAcceleration({frequency: 150}).subscribe(response => {
+        if(response.z > 6){
+          renderer.setStyle(el.nativeElement, 'transform', `translate(${response.x * 40}px,${response.y*-40}px)`);
+          if(Math.abs(response.x * 40) + Math.abs(response.y * 40) > 190){
+            renderer.setStyle(el.nativeElement,'display','none');
+          }else {
+            renderer.setStyle(el.nativeElement,'display','initial');
+          }
+          return;
         }
-        return;
-      }
-      renderer.setStyle(el.nativeElement,'display','initial');
-      if(screenOrientation.type=='landscape-primary'){
-        renderer.setStyle(el.nativeElement, 'transform', `translateX(${response[this.currentOrientation].toFixed(3) * -40}px)`);
-        return;
-      }
-      renderer.setStyle(el.nativeElement, 'transform', `translateX(${response[this.currentOrientation].toFixed(3) * 40}px)`);
+        renderer.setStyle(el.nativeElement,'display','initial');
+        if(screenOrientation.type=='landscape-primary'){
+          renderer.setStyle(el.nativeElement, 'transform', `translateX(${response[this.currentOrientation].toFixed(3) * -40}px)`);
+          return;
+        }
+        renderer.setStyle(el.nativeElement, 'transform', `translateX(${response[this.currentOrientation].toFixed(3) * 40}px)`);
+      });
+      screenOrientation.onChange().subscribe(response => {
+        if(screenOrientation.type.indexOf('portrait')){
+          this.currentOrientation = 'y';
+        }
+        if(screenOrientation.type.indexOf('landscape')){
+          this.currentOrientation = 'x';
+        }
+      });
     });
-    screenOrientation.onChange().subscribe(response => {
-      if(screenOrientation.type.indexOf('portrait')){
-        this.currentOrientation = 'y';
-      }
-      if(screenOrientation.type.indexOf('landscape')){
-        this.currentOrientation = 'x';
-      }
-    })
   }
   currentOrientation:string = 'x';
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { DeviceMotion } from '@ionic-native/device-motion/ngx';
+import { Platform } from '@ionic/angular';
 import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -8,14 +10,20 @@ import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
 })
 export class HomePage {
 
-  constructor(private deviceMotion: DeviceMotion, private screenOri: ScreenOrientation) {
-    deviceMotion.watchAcceleration({frequency:250}).subscribe(response => {
-      if(response.z > 6){
-        this.flat = true;
-        return;
-      }
-      this.flat = false;
-    });
+  constructor(private deviceMotion: DeviceMotion, private platform: Platform, public screenOrientation: ScreenOrientation) {
+    platform.ready().then(()=> {
+      deviceMotion.watchAcceleration({frequency:250}).subscribe(response => {
+        this.x = Math.abs(Math.round(Math.atan2(response.y, response.z) * 57.3));
+        this.y = Math.abs(Math.round(Math.atan2((-response.x), Math.sqrt(response.y*response.y + response.z*response.z))*57.3));
+        if(response.z > 6 && screenOrientation.type.indexOf('landscape')){
+          this.flat = true;
+          return;
+        }
+        this.flat = false;
+      });
+    })
   }
   flat:boolean;
+  x:number = 0;
+  y:number = 0;
 }
